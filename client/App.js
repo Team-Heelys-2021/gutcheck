b0c6d272e4460e1d7a62eb9ad6ab643a0ffc2153
Jack <jackvhall@outlook.com>
2021-09-25 12:32:56 -0700

    dotenv and autocomplete calls api

--- client/App.js
+++ client/App.js
@@ -5,46 +5,64 @@ import TextField from '@mui/material/TextField';
 import Auth from './pages/Auth'
 import './index.scss'
 
+import * as dataService from './foodDataService'
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false)
-  // return (
-  //   <React.Fragment>
-  //   <AppBar />
-  //   <div className="grid-container">
-  //     <div className="grid-item-1">
-  //       <Autocomplete
-  //         id="size-small-filled"
-  //         size="small"
-  //         options={[]}
-  //         getOptionLabel={(option) => option.title}
-  //         // defaultValue={}
-  //         renderTags={(value, getTagProps) =>
-  //           value.map((option, index) => (
-  //             <Chip
-  //               variant="outlined"
-  //               label={option.title}
-  //               size="small"
-  //               {...getTagProps({ index })}
-  //             />
-  //           ))
-  //         }
-  //         renderInput={(params) => (
-  //           <TextField
-  //             {...params}
-  //             variant="filled"
-  //             label="Size small"
-  //             placeholder="Favorites"
-  //           />
-  //         )}
-  //       />
-  //     </div>
-  //     <div className="grid-item-2">asdf</div>
-  //     <div className="grid-item-3">asdfasdf</div>
-  //   </div>
-  //   </React.Fragment>
-  // )
+  const [results, setResults] = React.useState([])
+  const [search, setSearch] = React.useState('')
+  
 
-  return <Auth logIn={(status) => setIsLoggedIn(status)}/>
+  React.useEffect(() => {
+    if (search.length > 1) {
+      searchFoods(search)
+    }
+  }, [search])
+
+  const searchFoods = async () => {
+    setResults(await dataService.searchFoods(search))
+  }
+
+  return (
+    <React.Fragment>
+    <AppBar />
+    <div className="grid-container">
+      <div className="grid-item-1">
+        <Autocomplete
+          id="size-small-filled"
+          size="small"
+          options={results}
+          getOptionLabel={(option) => `${option.description} ${option.brandName ? `(${option.brandName})` : ''}`}
+          // defaultValue={}
+          renderTags={(value, getTagProps) =>
+            value.map((option, index) => (
+              <Chip
+                variant="outlined"
+                label={option.title}
+                size="small"
+                {...getTagProps({ index })}
+              />
+            ))
+          }
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="filled"
+              label="Size small"
+              placeholder="Favorites"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+          )}
+        />
+      </div>
+      <div className="grid-item-2">asdf</div>
+      <div className="grid-item-3">asdfasdf</div>
+    </div>
+    </React.Fragment>
+  )
+
+  // return <Auth logIn={(status) => setIsLoggedIn(status)}/>
 }
 
 export default App
