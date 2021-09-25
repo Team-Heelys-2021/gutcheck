a972b44917ec88ff031c633863d9cbc4bb4a81f1
Ece Ozalp <eceiozalp@gmail.com>
2021-09-25 18:00:24 -0400

    sequelize models and first entries created

--- sql/models/Entry.js
+++ sql/models/Entry.js
@@ -10,6 +10,7 @@ module.exports = function (sequelize, Datatypes) {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
+        autoIncrement: true,
       },
       date: {
         type: DataTypes.DATEONLY, //Date Without time
--- sql/sequelize.js
+++ sql/sequelize.js
@@ -40,7 +40,7 @@ Entries.belongsTo(Foods); // adds a foodsId attribute to Entries to hold the pri
 
 //test case for creating users
 
-async function createUser() {
+async function createRecords() {
   await sequelize.sync({ force: true });
    //creates all the tables 
   const user = await Users.create({
@@ -52,8 +52,16 @@ async function createUser() {
 
   });
   console.log("auto created id", user.id);
+  const banana = await Foods.create({
+    fdcId:'1468381', 
+    foodName: 'banana'
+  });
+  const firstEntry = await Entries.create({
+    UserId: 1,
+    FoodFdcId:'1468381'
+  })
   const users = await Users.findAll();
   console.log(users.every(user => user instanceof Users)); // true
   console.log("All users:", JSON.stringify(users, null, 2));
 }
-createUser();
+createRecords();
