6e779c44d66c3255e92ed2cf1fc74aa1c09393c3
Jack <jackvhall@outlook.com>
2021-09-24 21:33:45 -0700

    material UI and basic elements on screen

---
+++ client/App.js
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import AppBar from './components/AppBar'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import './index.scss'
+
+function App() {
+  return (
+    <React.Fragment>
+    <AppBar />
+    <div className="grid-container">
+      <div className="grid-item-1">
+        <Autocomplete
+          id="size-small-filled"
+          size="small"
+          options={[]}
+          getOptionLabel={(option) => option.title}
+          // defaultValue={}
+          renderTags={(value, getTagProps) =>
+            value.map((option, index) => (
+              <Chip
+                variant="outlined"
+                label={option.title}
+                size="small"
+                {...getTagProps({ index })}
+              />
+            ))
+          }
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="filled"
+              label="Size small"
+              placeholder="Favorites"
+            />
+          )}
+        />
+      </div>
+      <div className="grid-item-2">asdf</div>
+      <div className="grid-item-3">asdfasdf</div>
+    </div>
+    </React.Fragment>
+  )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,14 @@ module.exports = {
           }
         }, 
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ]
+      }
     ]
   },
   resolve: {
