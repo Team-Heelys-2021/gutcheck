c6b386e912b8b25b3a9ff2188f6be6deb34bd1ad
Jack <jackvhall@outlook.com>
2021-09-25 11:01:31 -0700

    authentication UI starting point

--- client/App.js
+++ client/App.js
@@ -2,45 +2,49 @@ import * as React from 'react'
 import AppBar from './components/AppBar'
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import Auth from './pages/Auth'
 import './index.scss'
 
 function App() {
-  return (
-    <React.Fragment>
-    <AppBar />
-    <div className="grid-container">
-      <div className="grid-item-1">
-        <Autocomplete
-          id="size-small-filled"
-          size="small"
-          options={[]}
-          getOptionLabel={(option) => option.title}
-          // defaultValue={}
-          renderTags={(value, getTagProps) =>
-            value.map((option, index) => (
-              <Chip
-                variant="outlined"
-                label={option.title}
-                size="small"
-                {...getTagProps({ index })}
-              />
-            ))
-          }
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="filled"
-              label="Size small"
-              placeholder="Favorites"
-            />
-          )}
-        />
-      </div>
-      <div className="grid-item-2">asdf</div>
-      <div className="grid-item-3">asdfasdf</div>
-    </div>
-    </React.Fragment>
-  )
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+  // return (
+  //   <React.Fragment>
+  //   <AppBar />
+  //   <div className="grid-container">
+  //     <div className="grid-item-1">
+  //       <Autocomplete
+  //         id="size-small-filled"
+  //         size="small"
+  //         options={[]}
+  //         getOptionLabel={(option) => option.title}
+  //         // defaultValue={}
+  //         renderTags={(value, getTagProps) =>
+  //           value.map((option, index) => (
+  //             <Chip
+  //               variant="outlined"
+  //               label={option.title}
+  //               size="small"
+  //               {...getTagProps({ index })}
+  //             />
+  //           ))
+  //         }
+  //         renderInput={(params) => (
+  //           <TextField
+  //             {...params}
+  //             variant="filled"
+  //             label="Size small"
+  //             placeholder="Favorites"
+  //           />
+  //         )}
+  //       />
+  //     </div>
+  //     <div className="grid-item-2">asdf</div>
+  //     <div className="grid-item-3">asdfasdf</div>
+  //   </div>
+  //   </React.Fragment>
+  // )
+
+  return <Auth logIn={(status) => setIsLoggedIn(status)}/>
 }
 
 export default App
