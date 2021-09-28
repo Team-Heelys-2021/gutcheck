3a0f215386f9cba466a4185b7634a6582229d72c
Jack <jackvhall@outlook.com>
2021-09-27 17:42:32 -0700

    list-component

--- client/App.js
+++ client/App.js
@@ -7,10 +7,9 @@ import Auth from "./pages/Auth";
 import "./index.scss";
 
 import * as dataService from "./foodDataService";
-import { NoEncryptionGmailerrorredSharp } from "@mui/icons-material";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
   const [results, setResults] = React.useState([]);
   const [search, setSearch] = React.useState("");
   const [selectedValue, setSelectedValue] = React.useState(null)
@@ -35,52 +34,54 @@ function App() {
     setResults(await dataService.searchFoods(search));
   };
 
-
   const handleDeleteEntry = (fdcId) => {
     const newEntries = entries.filter(entry => entry.fdcId !== fdcId)
     setEntries(newEntries)
     // TODO: send entry DELETE request to back end
   }
 
-  return (
-    <React.Fragment>
-      <AppBar />
-      <div className="grid-container">
-        <div className="grid-item-1">
-          <Autocomplete
-            id="size-small-filled"
-            size="small"
-            clearOnBlur={true}
-            clearOnEscape={true}
-            options={results}
-            getOptionLabel={(option) =>
-              `${option.description} ${
-                option.brandName ? `(${option.brandName})` : ''
-              } - (${option.fdcId})`
-            }
-            onChange={(e, value) => setSelectedValue(value)}
-            value={selectedValue}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                variant="outlined"
-                label="Size small"
-                placeholder="Favorites"
-                onChange={(e) => setSearch(e.target.value)}
-                value={search}
-              />
-            )}
-          />
+  if (isLoggedIn) {
+    return (
+      <React.Fragment>
+        <AppBar />
+        <div className="grid-container">
+          <div className="grid-item-1">
+            <Autocomplete
+              id="size-small-filled"
+              size="small"
+              clearOnBlur={true}
+              clearOnEscape={true}
+              options={results}
+              getOptionLabel={(option) =>
+                `${option.description} ${
+                  option.brandName ? `(${option.brandName})` : ''
+                } - (${option.fdcId})`
+              }
+              onChange={(e, value) => setSelectedValue(value)}
+              value={selectedValue}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  label="Size small"
+                  placeholder="Favorites"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                />
+              )}
+            />
+          </div>
+          <div className="grid-item-2">sdfsdfds</div>
+          <div className="grid-item-3">
+            <EntryList entries={entries} deleteEntry={handleDeleteEntry}/>
+          </div>
         </div>
-        <div className="grid-item-2">sdfsdfds</div>
-        <div className="grid-item-3">
-          <EntryList entries={entries} deleteEntry={handleDeleteEntry}/>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  } else {
+    return <Auth />
+  }
 
-  // return <Auth logIn={(status) => setIsLoggedIn(status)}/>
 }
 
 export default App;
