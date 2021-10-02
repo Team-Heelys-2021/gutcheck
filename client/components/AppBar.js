8eb222c8decd98da76741c0abed1795f7350c0ed
Ece Ozalp <eceiozalp@gmail.com>
2021-10-02 13:42:27 -0400

    to dashboard and home functionality added

--- client/components/AppBar.js
+++ client/components/AppBar.js
@@ -8,10 +8,21 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/LogoutOutlined';
 import { useOktaAuth } from '@okta/okta-react';
+import DashboardIcon from '@mui/icons-material/DashboardOutlined';
+import { useHistory } from 'react-router-dom';
 
 export default function ButtonAppBar() {
   const { oktaAuth } = useOktaAuth();
   const logout = async () => oktaAuth.signOut();
+
+  let history = useHistory();
+  const toDashboard = () => {
+    history.replace('/dashboard');
+  };
+
+  const toHome = () => {
+    history.replace('/');
+  };
 
   return (
     <Box sx={{ flexGrow: 1, marginTop: 1 }}>
@@ -27,8 +38,14 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Gutcheck
+            <Button color="inherit" onClick={toHome}>
+              Gutcheck
+            </Button>
           </Typography>
+          <Button color="inherit" onClick={toDashboard}>
+            Dashboard
+            <DashboardIcon sx={{ ml: 1 }} />
+          </Button>
           <Button color="inherit" onClick={logout}>
             Logout
             <LogoutIcon sx={{ ml: 1 }} />
