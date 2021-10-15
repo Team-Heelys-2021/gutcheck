b4e16586cbb05f87106724880834abecaca712be
Charles Gyer <charlesgyer@gmail.com>
2021-10-14 17:37:39 -0700

    pulsing circle and app bar updates

---
+++ client/components/PulsingCircle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PulsingCircle() {
+  return <div class="pulsating-circle"></div>
+}
--- client/pages/Journal.js
+++ client/pages/Journal.js
@@ -2,10 +2,16 @@ import React, {useState, useEffect} from 'react';
 import Calendar from '../components/Calendar.js';
 import CalendarModal from '../components/CalendarModal.js';
 import axios from 'axios';
+import PulsingCircle from '../components/PulsingCircle.js';
+import StillCircle from '../components/StillCircle.js';
+import Button from '@mui/material/Button';
 
 const Journal = () => {
   const [currentDay, setCurrentDay] = useState(new Date());
   const [journalEntries, setJournalEntries] = useState([]);
+  
+  // below should be brought over to meditation page
+  const [showPulse, setShowPulse] = useState(false);
 
   useEffect(() => {
     async function fetchMyApi() {
@@ -31,8 +37,16 @@ const Journal = () => {
     }
   }
   
+  function onPulseClick() {
+    setShowPulse(!showPulse);
+  }
+
+  // below references to still and pulsing circle should be moved to meditation
   return <div className='page_calendar_container'>
     <p>Journal Page</p>
+    <Button onClick={onPulseClick}>Rythm</Button>
+    {showPulse && <PulsingCircle/>}
+    {!showPulse && <stillCircle/>}
     <CalendarModal/>
     <Calendar handleDayClick={handleDayClick}/>
   </div>
