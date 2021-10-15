1a0436c532300e4d2a8707f282bea521c56435a2
Jay Park <jay.park1921@gmail.com>
2021-10-15 09:28:36 +0900

    added pulse

--- client/pages/Meditation.js
+++ client/pages/Meditation.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Meditation_Component from '../components/Meditation_Component.js';
 import Container from '@mui/material/Container';
+import Pulse from '../components/Pulse.js';
 
 const Zen = () => {
   return (
@@ -8,6 +9,7 @@ const Zen = () => {
       <p>Meditation Page</p>
         <Container>
           <Meditation_Component />
+          <Pulse />
         </Container>
     </>
   );
